4245721b982eb4bba641ff6fe09fafa69a5a5c9e
jollyjerr <jollyjerr@gmail.com>
2019-10-19 17:14:41 -0600

    removes greeting

--- bot/bot.js
+++ bot/bot.js
@@ -10,14 +10,14 @@ class Footprint extends BotHelper {
         this.onMembersAdded(async (context, next) => {
             console.log('A new member has been added! ☺️')
 
-            const greeting = 'Hello there! My name is Footprint, and I am here to help you make sustainable decisions. Let\'s get you logged in, and then feel free to ask me questions about the environment and our relationship with it! Sound good ?'
+            // const greeting = 'Hello there! My name is Footprint, and I am here to help you make sustainable decisions. Let\'s get you logged in, and then feel free to ask me questions about the environment and our relationship with it! Sound good ?'
 
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity(greeting);
-                }
-            }
+            // const membersAdded = context.activity.membersAdded;
+            // for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+            //     if (membersAdded[cnt].id !== context.activity.recipient.id) {
+            //         await context.sendActivity(greeting);
+            //     }
+            // }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
